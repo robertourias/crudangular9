7c3237ece598edef9502458a4a27509b68ffeeda
Roberto Nicoletti <roberto.urias@gmail.com>
2020-06-14 16:28:16 -0300

    criar serviço api

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,18 +1,16 @@
+import { ApiService } from './services/api.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  providers: [HttpClientModule, ApiService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
+
--- front-end/src/app/services/api.service.ts
+++ front-end/src/app/services/api.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import 'rxjs/add/operator/map';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  caminho = 'http://localhost:86/crudangular9/back-end/';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  Api(dados: any, api: string) {
+    const httpOptions = {
+      headers: new HttpHeaders(
+        {
+          'Content-Type': 'application/json'
+        }
+      )
+    };
+
+    const url = this.caminho + api;
+    return this.http.post(url, JSON.stringify(dados), httpOptions).map(res => res);
+
+  }
+
+
 }
